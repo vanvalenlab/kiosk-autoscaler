6770bd9685d23e4e27c98b07af55b53a71c75b69
dylanbannon <dylanbannon5@gmail.com>
2019-01-21 12:54:43 -0800

    organized into functions

--- autoscale.sh
+++ autoscale.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
-DEBUG=${DEBUG:-true}
-INTERVAL=${INTERVAL:-5}
-REDIS_HOST=${REDIS_HOST:-redis-service}
-REDIS_PORT=${REDIS_PORT:-40960}
-#namespace=${NAMESPACE:-deepcell}
-#deployment=${DEPLOYMENT}
-#keysPerPod=5
-#minPods=${MIN_PODS:-0}
-#maxPods=${MAX_PODS:-4}
+function initialize_parameters() {
+  DEBUG=${DEBUG:-true}
+  INTERVAL=${INTERVAL:-5}
+  REDIS_HOST=${REDIS_HOST:-redis-service}
+  REDIS_PORT=${REDIS_PORT:-40960}
+  #namespace=${NAMESPACE:-deepcell}
+  #deployment=${DEPLOYMENT}
+  #keysPerPod=5
+  #minPods=${MIN_PODS:-0}
+  #maxPods=${MAX_PODS:-4}
+}
+
+function parse_parameters() {
+  autoscalingNoWS=$(echo "$AUTOSCALING" | tr -d "[:space:]")
+  IFS=';' read -ra autoscalingArr <<< "$autoscalingNoWS"
+  debug "$(date) -- debug -- autoscalingNoWS: $autoscalingNoWS"
+  debug "$(date) -- debug -- autoscalingArr[0]: ${autoscalingArr[0]}"
+  debug "$(date) -- debug -- autoscalingArr[1]: ${autoscalingArr[1]}"
+}
 
 function debug() {
   if [ "${DEBUG}" == "true" ]; then
@@ -41,17 +51,169 @@ function getCurrentPods() {
   echo ""
 }
 
+function verify_mins_and_maxes() {
+  # Determine how many pods we need, taking into account scaling limits.
+  desiredPods=""
+  # Flag used to prevent scaling down or up if currentPods are already min or max respectively.
+  scale=0
+  if [[ $requiredPods -le $minPods ]]; then
+    desiredPods=$minPods
+    # If currentPods are already at min, do not scale down
+    if [[ $currentPods -eq $minPods ]]; then
+      scale=1
+    fi
+  elif [[ $requiredPods -ge $maxPods ]]; then
+    desiredPods=$maxPods
+    # If currentPods are already at max, do not scale up
+    if [[ $currentPods -eq $maxPods ]]; then
+      scale=1
+    fi
+  else
+    desiredPods=$requiredPods
+  fi
+}  
 
+function scale_and_log() {
+  if [[ $scale -eq 0 ]]; then
+  # To slow down the scale-down policy, scale down in steps (reduce 10% on every iteration)
+  #if [[ $desiredPods -lt $currentPods ]]; then
+  #  desiredPods=$(awk "BEGIN { print int( ($currentPods - $desiredPods) * 0.9 + $desiredPods ) }")
+  #fi
+    scale_pods
+    log_scaling_result
+  fi
+}
 
-autoscalingNoWS=$(echo "$AUTOSCALING" | tr -d "[:space:]")
-IFS=';' read -ra autoscalingArr <<< "$autoscalingNoWS"
-debug "$(date) -- debug -- autoscalingNoWS: $autoscalingNoWS"
-debug "$(date) -- debug -- autoscalingArr[0]: ${autoscalingArr[0]}"
-debug "$(date) -- debug -- autoscalingArr[1]: ${autoscalingArr[1]}"
+function scale_pods() {
+  kubectl scale -n $namespace --replicas=$podTarget $resource_type/$deployment 1> /dev/null
+}
+
+function log_scaling_result() {
+  if [[ $? -eq 0 ]]; then
+    # Adjust logging and Slack notifications based on LOGS env and desiredPods number
+    log=false
+    avgPods=$(awk "BEGIN { print int( ($minPods + $maxPods) / 2 ) }")
+
+    if [[ $LOGS == "HIGH" ]]; then
+      log=true
+    elif [[ $LOGS == "MEDIUM" && ($desiredPods -eq $minPods || $desiredPods -eq $avgPods || $desiredPods -eq $maxPods) ]]; then
+      log=true
+    elif [[ $LOGS == "LOW" && ($desiredPods -eq $minPods || $desiredPods -eq $maxPods) ]]; then
+      log=true
+    fi
+
+    if $log ; then
+      echo "$(date) -- Scaled $deployment to $desiredPods pods ($queueMessages msg in the Redis queue)"
+    fi
+  else
+    echo "$(date) -- Failed to scale $deployment pods."
+  fi
+}
+
+function get_keys() {
+  # find out how many of the keys we retrieved were zip files, how many were
+  # images, and how many were, regardless of their file type, expired
+  imageKeys=0
+  zipKeys=0
+  for key in $queueKeys
+  do
+    key_status=$(redis-cli -h $REDIS_HOST -p $REDIS_PORT hget $key status)
+    if [[ "$key_status" != "done" && "$key_status" != "failed" ]]; then
+      file_name=$(redis-cli -h $REDIS_HOST -p $REDIS_PORT hget $key file_name)
+      if [[ $file_name =~ ^.+\.zip$ ]]; then
+        ((zipKeys++))
+      else
+        ((imageKeys++))
+      fi
+    else 
+      :
+    fi
+  done
+}
+
+function determine_required_pods() {
+  # and determine how many pods we need.
+  imagePods=$(echo "$imageKeys/$keysPerPod" | bc 2> /dev/null)
+  zipPods=$(echo "$zipKeys/$keysPerPod" | bc 2> /dev/null)
+
+  # Does this deployment revolve around zip files or image files?
+  # Note that the answer could vary for predictoin-related eployments, but htat 
+  # all training-related deployments use zip files.
+  if [ "$predict_or_train" == "predict" ]; then
+    if [[ "$deployment" == "zip-consumer-deployment" ]]; then
+      requiredPods=$zipPods
+    else
+      requiredPods=$imagePods
+    fi
+  else
+    requiredPods=$zipPods
+  fi
+
+  # If we don't have enough jobs to fill up an entire pod, should we still provision one?
+  # Yes, if we're talking about image prediction pods.
+  if [[ $predict_or_train == "predict" && $requiredPods -eq 0  &&  $imageKeys -gt 0 ]]; then
+    requiredPods=1
+  fi
+}
+
+function output_debug_info() {
+  debug "$(date) -- debug -- namespace: $namespace"
+  debug "$(date) -- debug -- resource type: $resource_type"
+  debug "$(date) -- debug -- predict or train: $predict_or_train"
+  debug "$(date) -- debug -- deployment name: $deployment"
+  if [[ "$deployment" == "zip-consumer-deployment" ]]; then
+    debug "$(date) -- debug -- number of keys: $zipeKeys"
+  else
+    debug "$(date) -- debug -- number of keys: $imageKeys"
+  fi 
+  debug "$(date) -- debug -- number of keys per pod: $keysPerPod"
+  if [[ "$deployment" == "zip-consumer-deployment" ]]; then
+    debug "$(date) -- debug -- number of required pods: $zipPods"
+  else
+    debug "$(date) -- debug -- number of required pods: $requiredPods"
+  fi
+  debug "$(date) -- debug -- max pods: $maxPods"
+  debug "$(date) -- debug -- min pods: $minPods"
+}
+
+function do_need_pods() {
+  # find out how many pods we've already requested.
+  currentPods=$(getCurrentPods)
+  debug "$(date) -- debug -- current number of pods: $currentPods"
+
+  # If we already have some pods requested
+  if [[ $currentPods != "" ]]; then
+    # and the amount we need is different from what we already have requested
+    if [[ "$requiredPods" -ne "$currentPods" ]]; then
+      debug "$(date) -- debug -- need to change numer of pods, but to what?"
+      # Determine how many pods we need, taking into account scaling limits.
+      verify_mins_and_maxes
+      # If appropriate, go ahead and scale.
+      scale_and_log
+    else
+      debug "$(date) -- debug -- apparently don't need more pods"
+    fi
+  else
+    echo "$(date) -- Failed to get current pods number for $deployment."
+  fi
+}
+
+function dont_need_pods() {
+  echo "$(date) -- Don't need any pods for $deployment."
+  if [[ $minPods -eq 0 ]]; then
+    desiredPods=$requiredPods
+    kubectl scale -n $namespace --replicas=$desiredPods $resource_type/$deployment 1> /dev/null
+    echo "$(date) -- So we scaled down to 0."
+  else
+    echo "$(date) -- But we have to keep a minimum number of pods."
+  fi
+}
+
 
 #
 # Main loop
 #
+function main_loop() {
 while true; do
 
   for autoscaler in "${autoscalingArr[@]}"; do
@@ -69,147 +231,18 @@ while true; do
   
     # then, if the last call was successful
     if [[ $? -eq 0 ]]; then
-  
-      # find out how many of the keys we retrieved were zip files, how many were
-      # images, and how many were, regardless of their file type, expired
-      imageKeys=0
-      zipKeys=0
-      for key in $queueKeys
-      do
-        key_status=$(redis-cli -h $REDIS_HOST -p $REDIS_PORT hget $key status)
-        if [[ "$key_status" != "done" && "$key_status" != "failed" ]]; then
-          file_name=$(redis-cli -h $REDIS_HOST -p $REDIS_PORT hget $key file_name)
-          if [[ $file_name =~ ^.+\.zip$ ]]; then
-            ((zipKeys++))
-          else
-            ((imageKeys++))
-          fi
-        else 
-          :
-        fi
-      done
-  
-      # and determine how many pods we need.
-      imagePods=$(echo "$imageKeys/$keysPerPod" | bc 2> /dev/null)
-      zipPods=$(echo "$zipKeys/$keysPerPod" | bc 2> /dev/null)
+      get_keys
+      determine_required_pods
+      output_debug_info
 
-      # Does this deployment revolve around zip files or image files?
-      # Note that the answer could vary for predictoin-related eployments, but htat 
-      # all training-related deployments use zip files.
-      if [ "$predict_or_train" == "predict" ]; then
-        if [[ "$deployment" == "zip-consumer-deployment" ]]; then
-          requiredPods=$zipPods
-        else
-          requiredPods=$imagePods
-        fi
-      else
-        requiredPods=$zipPods
-      fi
-
-      # If we don't have enough jobs to fill up an entire pod, should we still provision one?
-      # Yes, if we're talking about image prediction pods.
-      if [[ $predict_or_train == "predict" && $requiredPods -eq 0  &&  $imageKeys -gt 0 ]]; then
-        requiredPods=1
-      fi
-  
-      debug "$(date) -- debug -- namespace: $namespace"
-      debug "$(date) -- debug -- resource type: $resource_type"
-      debug "$(date) -- debug -- predict or train: $predict_or_train"
-      debug "$(date) -- debug -- deployment name: $deployment"
-      if [[ "$deployment" == "zip-consumer-deployment" ]]; then
-        debug "$(date) -- debug -- number of keys: $zipeKeys"
-      else
-        debug "$(date) -- debug -- number of keys: $imageKeys"
-      fi 
-      debug "$(date) -- debug -- number of keys per pod: $keysPerPod"
-      if [[ "$deployment" == "zip-consumer-deployment" ]]; then
-        debug "$(date) -- debug -- number of required pods: $zipPods"
-      else
-        debug "$(date) -- debug -- number of required pods: $requiredPods"
-      fi
-      debug "$(date) -- debug -- max pods: $maxPods"
-      debug "$(date) -- debug -- min pods: $minPods"
-
-
-      # Now, if we need one or more pods
+      # Now, do we need one or more pods?
       if [[ $requiredPods -ge 1 ]]; then
-  
-        # find out how many pods we've already requested.
-        currentPods=$(getCurrentPods)
-        debug "$(date) -- debug -- current number of pods: $currentPods"
-  
-        # If we alrady have some pods requested
-        if [[ $currentPods != "" ]]; then
-          # and the amount we need is different from what we already have requested
-          if [[ "$requiredPods" -ne "$currentPods" ]]; then
-              debug "$(date) -- debug -- need more pods"
-            # Determine how many pods we need, taking into account scaling limits.
-            desiredPods=""
-            # Flag used to prevent scaling down or up if currentPods are already min or max respectively.
-            scale=0
-            if [[ $requiredPods -le $minPods ]]; then
-              desiredPods=$minPods
-              # If currentPods are already at min, do not scale down
-              if [[ $currentPods -eq $minPods ]]; then
-                scale=1
-              fi
-            elif [[ $requiredPods -ge $maxPods ]]; then
-              desiredPods=$maxPods
-              # If currentPods are already at max, do not scale up
-              if [[ $currentPods -eq $maxPods ]]; then
-                scale=1
-              fi
-            else
-              desiredPods=$requiredPods
-            fi
-  
-            # If we are not constrained by scaling limits, then go ahead and scale.
-            if [[ $scale -eq 0 ]]; then
-              # To slow down the scale-down policy, scale down in steps (reduce 10% on every iteration)
-              if [[ $desiredPods -lt $currentPods ]]; then
-                desiredPods=$(awk "BEGIN { print int( ($currentPods - $desiredPods) * 0.9 + $desiredPods ) }")
-              fi
-  
-              kubectl scale -n $namespace --replicas=$desiredPods $resource_type/$deployment 1> /dev/null
-  
-              if [[ $? -eq 0 ]]; then
-                # Adjust logging and Slack notifications based on LOGS env and desiredPods number
-                log=false
-                avgPods=$(awk "BEGIN { print int( ($minPods + $maxPods) / 2 ) }")
-  
-                if [[ $LOGS == "HIGH" ]]; then
-                  log=true
-                elif [[ $LOGS == "MEDIUM" && ($desiredPods -eq $minPods || $desiredPods -eq $avgPods || $desiredPods -eq $maxPods) ]]; then
-                  log=true
-                elif [[ $LOGS == "LOW" && ($desiredPods -eq $minPods || $desiredPods -eq $maxPods) ]]; then
-                  log=true
-                fi
-  
-                if $log ; then
-                  echo "$(date) -- Scaled $deployment to $desiredPods pods ($queueMessages msg in the Redis queue)"
-                fi
-              else
-                echo "$(date) -- Failed to scale $deployment pods."
-              fi
-            fi
-          else
-            debug "$(date) -- debug -- apparently don't need more pods"
-          fi
-        else
-          echo "$(date) -- Failed to get current pods number for $deployment."
-        fi
+        do_need_pods
       else
-        echo "$(date) -- Don't need any pods for $deployment."
-        if [[ $minPods -eq 0 ]]; then
-          desiredPods=$requiredPods
-          kubectl scale -n $namespace --replicas=$desiredPods $resource_type/$deployment 1> /dev/null
-          echo "$(date) -- So we scaled down to 0."
-        else
-          echo "$(date) -- But we have to keep a minimum number of pods."
-        fi
+        dont_need_pods
       fi
     else
-      echo "$(date) -- Failed to get queue messages from $RABBIT_HOST for $deployment."
+      echo "$(date) -- Failed to get entries from Redis for $deployment."
     fi
     debug "$(date) -- debug --"
     debug "$(date) -- debug --"
@@ -228,3 +261,9 @@ while true; do
 
   sleep $INTERVAL
 done
+}
+
+### Code execution begins here. ###
+initialize_parameters
+parse_parameters
+main_loop
