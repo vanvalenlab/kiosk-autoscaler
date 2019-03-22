7183872196c670d287654b49b42d3d4e2381d3a5
William Graf <7930703+willgraf@users.noreply.github.com>
2019-03-21 17:57:09 -0700

    move autoscaler class into its own module

--- autoscale.py
+++ autoscale.py
@@ -1,257 +1,92 @@
-# autoscale.py
-# Sort the key in the Redis base according to name and scale different
-# Kubernetes deployments as appropriate.
+# Copyright 2016-2019 The Van Valen Lab at the California Institute of
+# Technology (Caltech), with support from the Paul Allen Family Foundation,
+# Google, & National Institutes of Health (NIH) under Grant U24CA224309-01.
+# All rights reserved.
+#
+# Licensed under a modified Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.github.com/vanvalenlab/kiosk-autoscaler/LICENSE
+#
+# The Work provided may be used for non-commercial academic purposes only.
+# For any other use of the Work, including commercial use, please contact:
+# vanvalenlab@gmail.com
+#
+# Neither the name of Caltech nor the names of its contributors may be used
+# to endorse or promote products derived from this software without specific
+# prior written permission.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+"""
+Sort the key in the Redis base according to name and scale different
+Kubernetes deployments as appropriate.
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
 
 import logging
-import os
-import re
-import subprocess
 import time
 import sys
 
-from redis import StrictRedis
-from redis.exceptions import ConnectionError
+import redis
 
-class Autoscaler():
-    def __init__(self):
-        # initialize variables
-        self.AUTOSCALING = os.environ['AUTOSCALING'].rstrip()
-        self.REDIS_HOST = os.environ['REDIS_HOST']
-        self.REDIS_PORT = os.environ['REDIS_PORT']
-        self.sleep_seconds = int(os.environ['INTERVAL'])
-        self.predict_keys = 0
-        self.train_keys = 0
+import autoscaler
 
-        # configure logger
-        self._configure_logger()
-        
-        # establish Redis connection
-        self.redis_client = StrictRedis(
-            host=self.REDIS_HOST,
-            port=self.REDIS_PORT,
-            decode_responses=True,
-            charset='utf-8')
-   
-        self._parse_autoscaling_variable()
 
-    def _configure_logger(self):
-        self.as_logger = logging.getLogger('autoscaler')
-        self.as_logger.setLevel(logging.DEBUG)
-        # Send logs to stdout so they can be read via Kubernetes.
-        sh = logging.StreamHandler(sys.stdout)
-        sh.setLevel(logging.DEBUG)
-        formatter = logging.Formatter(
-                '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        sh.setFormatter(formatter)
-        self.as_logger.addHandler(sh)
-        # Also send logs to a file for later inspection.
-        fh = logging.FileHandler('autoscaler.log')
+def initialize_logger(debug_mode=False):
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter('[%(asctime)s]:[%(levelname)s]:[%(name)s]: %(message)s')
+    console = logging.StreamHandler(stream=sys.stdout)
+    console.setFormatter(formatter)
+
+    fh = logging.FileHandler('redis-janitor.log')
+    fh.setFormatter(formatter)
+
+    if debug_mode:
+        console.setLevel(logging.DEBUG)
         fh.setLevel(logging.DEBUG)
-        formatter = logging.Formatter(
-                '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        fh.setFormatter(formatter)
-        self.as_logger.addHandler(fh)
+    else:
+        console.setLevel(logging.INFO)
+        fh.setLevel(logging.INFO)
 
-    def _parse_autoscaling_variable(self):
-        self.autoscaling_params = []
-        autoscales = self.AUTOSCALING.split(';')
-        for autoscale in autoscales:
-            autoscale_list = autoscale.split("|")
-            self.autoscaling_params.append(autoscale_list)
+    logger.addHandler(console)
+    logger.addHandler(fh)
 
-    def _make_kubectl_call(self, parameter_list):
-        while True:
-            try:
-                subprocess.run(parameter_list)
-                break
-            except CalledProcessError as err:
-                # For some reason, we can't execute this command right now.
-                # Keep trying until we can.
-                self._logger.warn("Trouble executing subprocess command " +
-                        "using parameters %s. Retrying. %s: %s",
-                        parameters_list, type(err).__name__, err)
-                time.sleep(5)
 
-    def _get_kubectl_output(self, parameter_list):
-        while True:
-            try:
-                pods_info = subprocess.check_output(parameter_list)
-                pods = pods_info.__str__()
-                break
-            except CalledProcessError as err:
-                # For some reason, we can't execute this command right now.
-                # Keep trying until we can.
-                self._logger.warn("Trouble executing subprocess command " +
-                        "using parameters %s. Retrying. %s: %s",
-                        parameters_list, type(err).__name__, err)
-                time.sleep(5)
-        return pods
+if __name__ == '__main__':
+    initialize_logger()
 
-    def _redis_keys(self):
-        self.as_logger.debug("Getting all Redis keys.")
-        while True:
-            try:
-                self.all_redis_keys = self.redis_client.keys()
-                break
-            except ConnectionError as err:
-                # For some reason, we're unable to connect to Redis right now.
-                # Keep trying until we can.
-                self.as_logger.warn("Trouble connecting to Redis. Retrying." +
-                        " %s: %s", type(err).__name__, err)
-                time.sleep(5)
-        self.as_logger.debug("Got all Redis keys.")
+    _logger = logging.getLogger(__file__)
 
-    def _redis_hgetall(self, key):
-        while True:
-            try:
-                key_values = self.redis_client.hgetall(key)
-                break
-            except ConnectionError as err:
-                # For some reason, we're unable to connect to Redis right now.
-                # Keep trying until we can.
-                self.as_logger.warn("Trouble connecting to Redis. Retrying." +
-                        " %s: %s", type(err).__name__, err)
-                time.sleep(5)
-        return key_values
+    REDIS_CLIENT = redis.StrictRedis(
+        host=os.getenv('REDIS_HOST'),
+        port=os.getenv('REDIS_PORT'),
+        decode_responses=True,
+        charset='utf-8')
 
-    def _tally_keys(self):
-        self.as_logger.debug("Tallying Redis keys.")
-        self.predict_keys = 0
-        self.train_keys = 0
-        for key in self.all_redis_keys:
-            if re.match('predict', key) or re.match('train', key):
-                key_values = self._redis_hgetall(key)
-                try:
-                    if key_values['status'] not in ["done", "failed"]:
-                        if re.match('predict', key):
-                            self.predict_keys += 1
-                        elif re.match('train', key):
-                            self.train_keys += 1
-                except KeyError:
-                    self.as_logger.warn("Key " + key + " had no status field: "
-                            + str(key_values))
-        self.as_logger.debug("Found " + str(self.predict_keys) +
-                " prediction keys and " + str(self.train_keys) +
-                " training keys.")
+    SCALER = autoscaler.Autoscaler(
+        redis_client=REDIS_CLIENT,
+        scaling_config=os.getenv('AUTOSCALING'),
+        backoff_seconds=os.getenv('REDIS_INTERVAL', '1'))
 
-    def _get_current_pods(self, namespace, resource_type, deployment):
-        if resource_type == "deployment":
-            pod_checking_keyword="desired"
-        elif resource_type == "job":
-            pod_checking_keyword="Completions"
-        else:
-            raise ValueError("The resource_type of " + str(resource_type) +
-                    " is unsuitable. You need to use either " +
-                    "\"deployment\" or \"job\".")
-        parameter_list = ["kubectl", "-n", namespace, "describe",
-                resource_type, deployment]
-        deployment_info = self._get_kubectl_output(parameter_list)
-        deployment_str = str(deployment_info)[2:-1]
-        dstr = deployment_str.encode('utf-8').decode('unicode_escape')
-        depl_info_list = dstr.split("\n")
-        
-        deployment_re = "Replicas:\s+([0-9]+) desired | [0-9]+ updated | " + \
-                "[0-9]+ total | [0-9]+ available | [0-9]+ unavailable"
-        job_re_one = "Completions:\s+([0-9]+)"
-        for line in depl_info_list:
-            if resource_type == "deployment":
-                potential_match_deployment = re.match(deployment_re, line)
-                if potential_match_deployment is not None:
-                    current_pods = potential_match_deployment.group(1)
-                    break
-            elif resource_type == "job":
-                potential_match_job_one = re.match(job_re_one, line)
-                # This works so long as we don't delete Redis keys after
-                # they've been procesed.
-                # If we do start deleting keys (queue system), then we'll need
-                # to also identify the "Succeeded" line and subtract that value
-                # from Completions.
-                if potential_match_job_one is not None:
-                    current_pods = potential_match_job_one.group(1)
-                    break
-        if resource_type == "job" and current_pods == "<unset>":
-            current_pods = 0
-        current_pods = int(current_pods)
-        return current_pods
+    INTERVAL = int(os.getenv('INTERVAL', '5'))
 
-    def _scale_deployments(self):
-        # entry schema: minPods maxPods keysPerPod namespace resource_type 
-        #               predict_or_train deployment
-        self.as_logger.debug("Scaling deployments according to Redis key "
-                + "tallies.")
-        redis_keys_for_this_deployment = 0
-        for entry in self.autoscaling_params:
-            # parse entry
-            try:
-                min_pods = int(entry[0])
-                max_pods = int(entry[1])
-                keys_per_pod = int(entry[2])
-                namespace = str(entry[3])
-                resource_type = str(entry[4])
-                predict_or_train = str(entry[5])
-                deployment = str(entry[6])
-            except IndexError:
-                self.as_logger.error("Autoscaling entry " + str(entry) +
-                        " is malformed.")
-            self.as_logger.debug("    Scaling " + str(deployment))
-            if predict_or_train == "predict":
-                redis_keys_for_this_deployment = self.predict_keys
-            elif predict_or_train == "train":
-                redis_keys_for_this_deployment = self.train_keys
-            else:
-                raise ValueError("The value for predict_or_train from " +
-                        str(entry) + " should be either \"predict\" or" +
-                        " \"train\", not " + str(predict_or_train) + ".")
-
-            # compute desired pods for this deployment
-            desired_pods = redis_keys_for_this_deployment / keys_per_pod
-            # i had to ditch the round( x - 0.5) approach, due to "banker's rounding"
-            desired_pods = int(desired_pods // 1)
-            if desired_pods > max_pods:
-                desired_pods = max_pods
-            if desired_pods < min_pods:
-                desired_pods = min_pods
-
-            # prevent intermediate scaledown
-            # Since we can't direct the autoscaler to only remove idle pods,
-            # we need to wait until all pods are idle to remove any of them.
-            try:
-                current_pods = self._get_current_pods(namespace, resource_type,
-                        deployment)
-            except ValueError as ex:
-                raise ValueError("Error in autoscaling entry " + str(entry) +
-                        " as follows: " + ex)
-            if desired_pods < current_pods and desired_pods > 0:
-                desired_pods = current_pods
-
-            # scale pods, if necessary
-            # turning off scaling for training-job until I figure out a
-            # suitable method for scaling jobs
-            if resource_type == "job":
-                self.as_logger.debug("    Scaling has been disabled for jobs.")
-            else:
-                if desired_pods != current_pods:
-                    parameter_list = ["kubectl", "scale", "-n", namespace,
-                        "--replicas=" + str(desired_pods), resource_type +
-                        "/" + deployment]
-                    self._make_kubectl_call(parameter_list)
-                    self.as_logger.debug("    Scaled " + deployment + " to " +
-                            str(desired_pods) + " pods." )
-                else:
-                    self.as_logger.debug("    Deployment " + deployment + 
-                            " stays at " + str(current_pods) + " pods." )
-
-    def redis_monitoring_loop(self):
-        self.as_logger.debug("Entering autoscaling loop.")
-        while True:
-            self._redis_keys()
-            self._tally_keys()
-            self._scale_deployments()
-            self.as_logger.debug("Sleeping for " + str(self.sleep_seconds) +
-                    " seconds.")
-            time.sleep(self.sleep_seconds)
-            self.as_logger.debug(" ")
-
-if __name__=='__main__':
-    autoscaler = Autoscaler()
-    autoscaler.redis_monitoring_loop()
+    while True:
+        try:
+            SCALER.scale()
+            _logger.debug('Sleeping for %s seconds.', INTERVAL)
+            time.sleep(INTERVAL)
+        except Exception as err:  # pylint: disable=broad-except
+            _logger.critical('Fatal Error: %s: %s', type(err).__name__, err)
+            sys.exit(1)
---
+++ autoscaler/autoscaler.py
@@ -0,0 +1,267 @@
+# Copyright 2016-2019 The Van Valen Lab at the California Institute of
+# Technology (Caltech), with support from the Paul Allen Family Foundation,
+# Google, & National Institutes of Health (NIH) under Grant U24CA224309-01.
+# All rights reserved.
+#
+# Licensed under a modified Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.github.com/vanvalenlab/kiosk-autoscaler/LICENSE
+#
+# The Work provided may be used for non-commercial academic purposes only.
+# For any other use of the Work, including commercial use, please contact:
+# vanvalenlab@gmail.com
+#
+# Neither the name of Caltech nor the names of its contributors may be used
+# to endorse or promote products derived from this software without specific
+# prior written permission.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import re
+import time
+import logging
+import subprocess
+
+import redis
+
+
+class Autoscaler(object):
+    """Read Redis and scale up k8s pods if required"""
+
+    def __init__(self, redis_client, scaling_config, backoff_seconds=1,
+                 deployment_delim=';', param_delim='|'):
+        self.redis_client = redis_client
+        self.backoff_seconds = backoff_seconds
+        self.predict_keys = 0
+        self.train_keys = 0
+        self.logger = logging.getLogger(str(self.__class__.__name__))
+        self.completed_statuses = {'done', 'failed'}
+
+        self.autoscaling_params = self._get_autoscaling_params(
+            scaling_config=scaling_config.rstrip(),
+            deployment_delim=deployment_delim,
+            param_delim=param_delim)
+
+        self.redis_keys = {
+            'predict': 0,
+            'train': 0
+        }
+
+    def _get_autoscaling_params(self, scaling_config,
+                                deployment_delim=';',
+                                param_delim='|'):
+        if deployment_delim == param_delim:
+            raise ValueError('`deployment_delim` and `param_delim` must be '
+                             'different. Got "{}" and "{}".'.format(
+                                 deployment_delim, param_delim))
+
+        return [x.split(param_delim)
+                for x in scaling_config.split(deployment_delim)]
+
+    def _make_kubectl_call(self, parameter_list):
+        self.logger.debug('Executing subprocess: `%s`.',
+                          ' '.join(parameter_list))
+        while True:
+            try:
+                subprocess.run(parameter_list)
+                break
+            except subprocess.CalledProcessError as err:
+                # Error during subprocess.  Log it and retry after `backoff
+                self.logger.warning('Encountered "%s: %s" during subprocess '
+                                    'command: `%s`.  Retrying in %s seconds...',
+                                    type(err).__name__, err,
+                                    ' '.join(parameter_list),
+                                    self.backoff_seconds)
+                time.sleep(self.backoff_seconds)
+        return True
+
+    def _get_kubectl_output(self, parameter_list):
+        self.logger.debug('Executing subprocess: `%s`.',
+                          ' '.join(parameter_list))
+        while True:
+            try:
+                kubectl_output = subprocess.check_output(parameter_list)
+                kubectl_output = kubectl_output.__str__()
+                break
+            except subprocess.CalledProcessError as err:
+                # Error during subprocess.  Log it and retry after `backoff`
+                self.logger.warning('Encountered "%s: %s" during subprocess '
+                                    'command: `%s`.  Retrying in %s seconds...',
+                                    type(err).__name__, err,
+                                    ' '.join(parameter_list),
+                                    self.backoff_seconds)
+
+                time.sleep(self.backoff_seconds)
+        return kubectl_output
+
+    def scan_iter(self, match=None):
+        while True:
+            try:
+                response = self.redis_client.scan_iter(match=match)
+                break
+            except redis.exceptions.ConnectionError as err:
+                self.logger.warning('Encountered %s: %s when calling SCAN. '
+                                    'Retrying in %s seconds.',
+                                    type(err).__name__, err,
+                                    self.backoff_seconds)
+                time.sleep(self.backoff_seconds)
+        return response
+
+    def hget(self, rhash, key):
+        while True:
+            try:
+                response = self.redis_client.hget(rhash, key)
+                break
+            except redis.exceptions.ConnectionError as err:
+                self.logger.warning('Encountered %s: %s when calling HGET. '
+                                    'Retrying in %s seconds.',
+                                    type(err).__name__, err,
+                                    self.backoff_seconds)
+                time.sleep(self.backoff_seconds)
+        return response
+
+    def tally_keys(self):
+        # reset the key tallies to 0
+        for k in self.redis_keys:
+            self.redis_keys[k] = 0
+
+        for key in self.scan_iter():
+            if any(re.match(k, key) for k in self.redis_keys):
+                status = self.hget(key, 'status')
+
+                if status is None:
+                    self.logger.warning('Key %s had no `status` field.', key)
+                    continue
+
+                # add up each type of key that is "in-progress" or "new"
+                elif status not in self.completed_statuses:
+                    for k in self.redis_keys:
+                        if re.match(k, key):
+                            self.redis_keys[k] += 1
+
+        self.logger.debug('Finished tallying redis keys: %s', self.redis_keys)
+
+    def get_current_pods(self, namespace, resource_type, deployment):
+        """Find the number of current pods deployed for the given resource"""
+        # if resource_type  == 'deployment':
+        #     pod_checking_keyword = 'desired'
+        # elif resource_type == 'job':
+        #     pod_checking_keyword = 'Completions'
+        # else:
+        if resource_type not in {'deployment', 'job'}:
+            raise ValueError('The resource_type of {} is unsuitable. Use either'
+                             '`deployment` or `job`'.format(resource_type))
+
+        deployment_re = r'Replicas:\s+([0-9]+) desired | [0-9]+ updated | ' + \
+                        r'[0-9]+ total | [0-9]+ available | [0-9]+ unavailable'
+
+        description = self._get_kubectl_output([
+            'kubectl', '-n', namespace, 'describe', resource_type, deployment
+        ])
+
+        dstr = str(description)[2:-1].encode('utf-8').decode('unicode_escape')
+        for line in dstr.split('\n'):
+            if resource_type == 'deployment':
+                potential_match = re.match(deployment_re, line)
+                if potential_match is not None:
+                    current_pods = potential_match.group(1)
+                    break
+
+            elif resource_type == 'job':
+                potential_match = re.match(r'Completions:\s+([0-9]+)', line)
+                # This works so long as we don't delete Redis keys after
+                # they've been procesed.
+                # If we do start deleting keys (queue system), then we'll need
+                # to also identify the "Succeeded" line and subtract that value
+                # from Completions.
+                if potential_match is not None:
+                    current_pods = potential_match.group(1)
+                    break
+
+        if resource_type == 'job' and current_pods == '<unset>':
+            current_pods = 0
+
+        self.logger.debug('%s %s in namespace %s currently has %s pods.',
+                          str(resource_type).capitalize(), deployment,
+                          namespace, current_pods)
+
+        return int(current_pods)
+
+    def get_desired_pods(self, key, keys_per_pod, min_pods, max_pods, current_pods):
+        desired_pods = self.redis_keys[key] // keys_per_pod
+
+        # set `desired_pods` to inside the max/min boundaries.
+        if desired_pods > max_pods:
+            desired_pods = max_pods
+        elif desired_pods < min_pods:
+            desired_pods = min_pods
+
+        # To avoid removing currently running pods, wait until all
+        # pods of the deployment are idle before scaling down.
+        if 0 < desired_pods < current_pods:
+            desired_pods = current_pods
+
+        return desired_pods
+
+    def _scale_deployments(self):
+        for entry in self.autoscaling_params:
+            # entry schema: minPods maxPods keysPerPod namespace resource_type
+            #               predict_or_train deployment
+            try:
+                min_pods = int(entry[0])
+                max_pods = int(entry[1])
+                keys_per_pod = int(entry[2])
+                namespace = str(entry[3])
+                resource_type = str(entry[4])
+                predict_or_train = str(entry[5])
+                deployment = str(entry[6])
+            except IndexError:
+                self.logger.error('Autoscaling entry %s is malformed.', entry)
+                continue
+
+            self.logger.debug('\tScaling %s', deployment)
+
+            current_pods = self.get_current_pods(
+                namespace, resource_type, deployment)
+
+            # compute desired pods for this deployment
+            desired_pods = self.get_desired_pods(
+                predict_or_train, keys_per_pod,
+                min_pods, max_pods, current_pods)
+
+            # scale pods, if necessary
+            if resource_type == 'job':
+                # TODO: Find a suitable method for scaling jobs
+                self.logger.debug('Scaling has been disabled for Jobs.')
+
+            elif resource_type == 'deployment':
+                if desired_pods != current_pods:
+                    self._make_kubectl_call([
+                        'kubectl', 'scale', '-n', namespace,
+                        '--replicas={}'.format(desired_pods),
+                        '{}/{}'.format(resource_type, deployment)
+                    ])
+                    self.logger.debug('\tScaled %s to %s pods.',
+                                      deployment, desired_pods)
+                else:
+                    self.logger.debug('\tDeployment %s stays at %s pods.',
+                                      deployment, current_pods)
+
+            else:
+                raise ValueError('The resource_type of {} is unsuitable. '
+                                 'Use either `deployment` or `job`'.format(
+                                     resource_type))
+
+    def scale(self):
+        self.tally_keys()
+        self._scale_deployments()
