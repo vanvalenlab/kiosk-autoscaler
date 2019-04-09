2254ab2024fa3f239ad3d7c8a5804d0db9c2e6c1
William Graf <7930703+willgraf@users.noreply.github.com>
2019-04-09 16:51:30 -0700

    add docstring

--- autoscaler/autoscaler.py
+++ autoscaler/autoscaler.py
@@ -23,6 +23,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ============================================================================
+"""Autoscaler class"""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
