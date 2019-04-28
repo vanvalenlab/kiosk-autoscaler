76878b824eb4bf8a519c523d8200270cb5fcb933
Enrico Borba <enricozb@gmail.com>
2019-04-27 18:05:03 -0700

    added one autoscaler test

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
-python3 -m pip install --user -r requirements.txt
-python3 -m pip install --user pytest pytest-cov==2.5.1 pytest-pep8 coveralls
+python3 -m pip install -r requirements.txt
+python3 -m pip install pytest pytest-cov==2.5.1 pytest-pep8 coveralls
 
 python3 -m pytest --cov=autoscaler --pep8 autoscaler
 
