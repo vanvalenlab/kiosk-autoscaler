00be7263d77664ae2c918fa48d56cf57fc4252e3
Enrico Borba <enricozb@gmail.com>
2019-05-22 13:40:58 -0700

    Removed testing nrc scripts

--- build-and-push.sh
+++
@@ -1,3 +0,0 @@
-docker build -t vanvalenlab/kiosk-autoscaler:tracking-0.1 .
-docker push vanvalenlab/kiosk-autoscaler:tracking-0.1
-
--- test.sh
+++
@@ -1,5 +0,0 @@
-python3 -m pip install -r requirements.txt
-python3 -m pip install pytest pytest-cov==2.5.1 pytest-pep8 coveralls
-
-python3 -m pytest --cov=autoscaler --pep8 autoscaler
-
